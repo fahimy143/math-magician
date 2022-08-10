a45e878b07b1d81a32fb4b32908f9355fb73e733
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2022-08-11 01:56:29 +0430

    require changes done

--- src/logic/__test__/Calculate.test.js
+++ src/logic/__test__/Calculate.test.js
@@ -22,4 +22,14 @@ test('Testing calculate functions', () => {
     operation: '%',
     total: '0',
   });
+  expect(calculate({ next: '10', operation: '÷', total: '10' }, '÷')).toEqual({
+    next: null,
+    operation: '÷',
+    total: '1',
+  });
+  expect(calculate({ next: '1', operation: '-', total: '1' }, '-')).toEqual({
+    next: null,
+    operation: '-',
+    total: '0',
+  });
 });
