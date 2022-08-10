bee55c9b350ab5c37f80e99bc2358b8d2e43c8ab
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2022-08-11 00:24:47 +0430

    fixed linters

--- src/components/__test__/Calculator.test.js
+++ src/components/__test__/Calculator.test.js
@@ -3,6 +3,6 @@ import renderer from 'react-test-renderer';
 import Calculator from '../Calculator';
 
 it('renders when there is item', () => {
-    const tree = renderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
